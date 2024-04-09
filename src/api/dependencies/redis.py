27e783c7c6a8bae2263864a4d1d328d57f4b2d43
Sniffr <sidney@asilimia.co.ke>
2024-04-09 15:09:41 +0300

    removed unnecessary items

--- src/api/dependencies/redis.py
+++
@@ -1,6 +0,0 @@
-from redis.asyncio import Redis
-from starlette.requests import Request
-
-
-def get_redis_connection(request: Request) -> Redis:
-    return request.app.state.redis
--- src/key.py
+++
@@ -1,8 +0,0 @@
-# create secret signing key from cryptogrsphy fernet
-from cryptography.fernet import Fernet
-
-# Generate a key
-key = Fernet.generate_key()
-
-# Print the key
-print(key)
